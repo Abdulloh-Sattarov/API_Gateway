1cb5f2b3fb51c82411aaa375bba844e60991c4ae
Abdulloh <developer15pro@gmail.com>
2022-01-07 17:58:22 +0500

    Finish Book Swagger

--- api/handlers/models/catalog.go
+++ api/handlers/models/catalog.go
@@ -22,3 +22,45 @@ type UpdateCategory struct {
 type ListCategories struct {
 	Categories []Category `json:"categories"`
 }
+
+type Book struct {
+	BookId       string  `json:"BookId"`
+	Name         string  `json:"Name"`
+	AuthorId     string  `json:"AuthorId"`
+	Price        float64 `json:"Price"`
+	CategoryId   string  `json:"CategoryId"`
+	CategoryName string  `json:"CategoryName"`
+	CreatedAt    string  `json:"CreatedAt"`
+	UpdatedAt    string  `json:"UpdatedAt"`
+}
+
+type CreateBook struct {
+	Name       string  `json:"Name"`
+	AuthorId   string  `json:"AuthorId"`
+	Price      float64 `json:"Price"`
+	CategoryId string  `json:"CategoryId"`
+}
+
+type UpdateBook struct {
+	Name       string  `json:"Name"`
+	AuthorId   string  `json:"AuthorId"`
+	Price      float64 `json:"Price"`
+	CategoryId string  `json:"CategoryId"`
+}
+
+type ListBooks struct {
+	Books []Book `json:"Books"`
+}
+
+type Author struct {
+	AuthorId  string `json:"AuthorId"`
+	Name      string `json:"Name"`
+	CreatedAt string `json:"CreatedAt"`
+	UpdatedAt string `json:"UpdatedAt"`
+}
+
+type List struct {
+	Author     Author     `json:"Author"`
+	Book       Book       `json:"Book"`
+	Categories []Category `json:"Categories"`
+}
